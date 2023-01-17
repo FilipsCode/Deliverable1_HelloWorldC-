354a2630a4e2b5fe9d6d2508938a73862f3d871d
Filip Petersson <122743947+FilipsCode@users.noreply.github.com>
2023-01-17 17:45:00 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Deliverable1_Helloworld;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        string stg = "Hello, World! (2)";
+        Console.WriteLine(stg);
+
+
+    }
+}
